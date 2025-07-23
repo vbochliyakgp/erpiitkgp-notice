e547cdee0f4b5110f3334375efa00013c192e1df
vbochliyakgp <vbochliya@kgpian.iitkgp.ac.in>
2025-07-23 09:19:27 +0530

    origin true

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true,
     optionsSuccessStatus: 200,
   })
